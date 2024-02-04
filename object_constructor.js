610242cfebf3e927f0e9e42ef3978b247621cdae
Ryan Boudwin <rboudwin@student.hive.fi>
2024-02-04 11:50:17 +0200

    starting library exercise

--- object_constructor.js
+++ object_constructor.js
@@ -13,3 +13,24 @@ function Book(title, author, pages, read)
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
 console.log(theHobbit.info);
+
+let head = {
+	glasses: 1
+  };
+  
+  let table = {
+	pen: 3,
+	__proto__: head
+  };
+  
+  let bed = {
+	sheet: 1,
+	pillow: 2,
+	__proto__: table
+  };
+  
+  let pockets = {
+	money: 2000,
+	__proto__: bed
+  };
+  console.log("Hello " + pockets.glasses);
