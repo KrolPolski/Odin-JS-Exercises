257cdd9c05cb8936e5c0e01e7e9001322a09d215
Ryan Boudwin <rboudwin@student.hive.fi>
2024-02-29 16:43:30 +0200

    three books added with function

--- script.js
+++ script.js
@@ -15,6 +15,16 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
 	book = new Book(title, author, pages, read);
 	myLibrary.push(book);
+	let row = bookTable.insertRow(-1);
+	let c1 = row.insertCell(0);
+	let c2 = row.insertCell(1);
+	let c3 = row.insertCell(2);
+	let c4 = row.insertCell(3);
+
+	c1.innerText = myLibrary[myLibrary.length - 1].title;
+	c2.innerText = myLibrary[myLibrary.length - 1].author;
+	c3.innerText = myLibrary[myLibrary.length - 1].pages;
+	c4.innerText = myLibrary[myLibrary.length - 1].read;
 }
 let bookTable = document.querySelector("#bookTable");
 let newBookBtn = document.querySelector("#newBookBtn");
@@ -23,16 +33,7 @@ newBookBtn.addEventListener('click', () => {
 	newBookForm.style.display = 'block';
 });
 
-addBookToLibrary("Lord of the Rings", "JRR Tolkien", 156, true);
-/*bookTable = data.map(myLibrary => {
-	return (
-		<tr>
-			<td>${myLibrary.title}</td>
-			<td>${myLibrary.author}</td>
-			<td>${myLibrary.pages}</td>
-			<td>${myLibrary.read}</td>
-		</tr>
-	)
-});*/
-
+addBookToLibrary("Lord of the Rings", "JRR Tolkien", 1216, true);
+addBookToLibrary("Mistborn", "Brandon Sanderson", 541, true);
+addBookToLibrary("Ender's Game", "Orson Scott Card", 256, true);
 
