17688a3da4e08f16b7a44f4eca8328dfd67c7ef2
Ryan Boudwin <rboudwin@student.hive.fi>
2024-02-29 16:23:25 +0200

    Form now displays onclick

--- script.js
+++ script.js
@@ -4,11 +4,35 @@ function Book(title, author, pages, read) {
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
-	this.read = read;
+	if (read)
+		this.read = "already read";
+	else
+		this.read = "not read yet";
+	this.info = title + " by " + author + ", " + pages + " pages, " + this.read + "\n";
+	return this.info;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages, read) {
 	book = new Book(title, author, pages, read);
 	myLibrary.push(book);
 }
+let bookTable = document.querySelector("#bookTable");
+let newBookBtn = document.querySelector("#newBookBtn");
+let newBookForm = document.querySelector("#newBookForm");
+newBookBtn.addEventListener('click', () => {
+	newBookForm.style.display = 'block';
+});
+
+addBookToLibrary("Lord of the Rings", "JRR Tolkien", 156, true);
+/*bookTable = data.map(myLibrary => {
+	return (
+		<tr>
+			<td>${myLibrary.title}</td>
+			<td>${myLibrary.author}</td>
+			<td>${myLibrary.pages}</td>
+			<td>${myLibrary.read}</td>
+		</tr>
+	)
+});*/
+
 
